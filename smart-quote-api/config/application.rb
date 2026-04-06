2ffc6a7ea8be89c75f975dfbe08797af5ce59fdc
Jaehong <jhlim725@gmail.com>
2026-04-07 00:22:27 +0900

    CRITICAL WORKAROUND: Force fully permissive CORS via origins '*'

--- smart-quote-api/config/application.rb
+++ smart-quote-api/config/application.rb
@@ -42,7 +42,5 @@ module SmartQuoteApi
     config.api_only = true
 
     # Custom CORS Rack middleware - handles OPTIONS preflight at lowest level
-    require_relative "../lib/cors_rack_middleware"
-    config.middleware.insert_before 0, CorsRackMiddleware
   end
 end
--- smart-quote-api/config/initializers/cors.rb
+++ smart-quote-api/config/initializers/cors.rb
@@ -1,10 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    if ENV["CORS_ORIGINS"].present?
-      origins(*ENV["CORS_ORIGINS"].split(",").map(&:strip))
-    else
-      origins '*'
-    end
+    origins '*'
+
     
     resource "*",
       headers: :any,
