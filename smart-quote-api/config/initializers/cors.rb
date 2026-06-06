3144d51a9c9d95d23b1e0cb25c3eae0d1403ee69
Jaehong <jhlim725@gmail.com>
2026-06-06 11:48:00 +0900

    Update cors.rb

--- smart-quote-api/config/initializers/cors.rb
+++ smart-quote-api/config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       expose: [ "Authorization" ],
-      credentials: false,
+      credentials: true,
       max_age: 600
   end
 end
